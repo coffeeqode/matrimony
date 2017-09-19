7248ed6f3e60cf3afeff76f8c6575ee12ccec5c0
Sandesh Patil <sandesh89@gmail.com>
2017-09-19 22:54:44 +0530

    Bootstrap added. new component added for search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
